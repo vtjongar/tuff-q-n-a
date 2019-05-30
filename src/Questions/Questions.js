b810fa119b674a987d5bc99abaeded21c6c58d06
Vanessa Henderson <bookvanny@gmail.com>
2019-05-30 12:31:34 -0400

    added question cards using bootstrap

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -21,7 +21,26 @@ class Questions extends Component {
   render() {
     return (
       <div className="container">
-       
+        <div className="row">
+          {this.state.questions === null && <p>Loading...</p>}
+
+          {
+            this.state.questions && this.state.questions.map(question => (
+              <div key={question.id} className="col-sm-18 col-md-6 col-lg-5">
+                <Link to={`/question/${question.id}`}>
+                  <div className="card text-white bg-success mb-3">
+                    <div className="card-header">Answers: {question.answers}</div>
+                    <div className="card-body">
+                      <h4 className="card-title">{question.title}</h4>
+                      <p className="card-text">{question.description}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+          
+        </div>
       </div>
     )
   }
