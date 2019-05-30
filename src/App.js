e5630de63bf86ea71a5a2d566957c8e898ca3234
Vanessa Henderson <bookvanny@gmail.com>
2019-05-29 22:02:04 -0400

    added nav bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from './NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
+       <NavBar/>
       <h1>Testing this TUFF! app</h1>
     </div>
   );
